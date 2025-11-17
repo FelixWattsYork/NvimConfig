3a6f4e5337d805c9609ef2f4093d19a20952dc42
FelixWattsYork <hmq514@york.ac.uk>
2025-11-17 10:56:53 +0000

    might have added things

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -16,21 +16,6 @@ return {
     end,
   },
   {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
-    },
-    ft = "python", -- Load when opening Python files
-    keys = {
-      { ",v", "<cmd>VenvSelect<cr>" }, -- Open picker on keymap
-    },
-    opts = { -- this can be an empty lua table - just showing below for clarity.
-      search = {}, -- if you add your own searches, they go here.
-      options = {}, -- if you add plugin options, they go here.
-    },
-  },
-  {
     "3rd/image.nvim",
     build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
     opts = {
